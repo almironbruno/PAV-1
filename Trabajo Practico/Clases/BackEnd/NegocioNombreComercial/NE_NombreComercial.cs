888706d86bf4f3ba763fe389dcde0daa4255543d
miliarduino <miliarduino4243@gmail.com>
2022-10-04 12:17:53 -0300

    Lo que tengo de NC falta la modificacion

--- Trabajo Practico/Clases/BackEnd/NegocioNombreComercial/NE_NombreComercial.cs
+++ Trabajo Practico/Clases/BackEnd/NegocioNombreComercial/NE_NombreComercial.cs
@@ -7,26 +7,48 @@ using Trabajo_Practico.Clases.Entidades;
 
 namespace Trabajo_Practico.Clases.BackEnd.NegocioNombreComercial
 {
-    class NE_NombreComercial
+    internal class NE_NombreComercial
     {
-        BE_Acceso_Datos _BD = new BE_Acceso_Datos();
-        public void AgregarNombreComercial(NombresComerciales nombrecomercial)
-        {
-            string sqlInsertar = @"INSERT INTO nombres_comerciales"
-                                 + "(nombre_comercial, id_marca, id_gama)"
-                                 + "VALUES('"
-                                 + nombrecomercial.nombre_comercial + "'"
-                                 + ", '" + nombrecomercial.id_marca + "'"
-                                 + ", '" + nombrecomercial.id_gama + "')";
-                                
 
-            _BD.Insertar(sqlInsertar);
+        BE_Acceso_Datos _BD = new BE_Acceso_Datos();
+
+
+        public void AgregarNombreComercial(NombresComerciales nombrecomercialAgregar)
+        {
+            string sqlInsertar = $"INSERT INTO nombres_comerciales" + "(nombre_comercial, id_marca, id_gama)"
+                                 + " VALUES(' "
+                                 + nombrecomercialAgregar.nombre_comercial + "'"
+                                 + ", '" + nombrecomercialAgregar.marca + "'"
+                                 + ", '" + nombrecomercialAgregar.gama + "')";
+
+            _BD.ejecutar(sqlInsertar);
         }
 
-        public void BorrarNombreComercial(string nombre_comercial)
+
+        public void BorrarNombreComercial(string nom)
         {
-            string sqlBorrar = "DELETE FROM nombres_comerciales WHERE nombre_comercial = '" + nombre_comercial + "'";
+
+            string sqlBorrar = "DELETE FROM nombres_comerciales WHERE nombre_comercial = '" + nom + "'";
             _BD.Eliminar(sqlBorrar);
+        }
+
+
+        public void ModificarNombreComercial(NombresComerciales nomcomMod, string nombreViejo)
+        {                
+        }
+
+        public string consultaNomCom(string nomcom, string marc, string gam)
+        {
+
+            string sqlConsulta = $@"SELECT * FROM nombres_comeciales WHERE
+                                 nombre_comercial LIKE '{nomcom}'
+                                 AND id_marca LIKE '{marc}' AND
+                                 id_gama LIKE '{gam}'";
+
+            return sqlConsulta;
         }
     }
 }
+
+    
+
